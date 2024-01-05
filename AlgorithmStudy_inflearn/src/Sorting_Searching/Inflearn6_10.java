c7ca5f907a0884b1817e56d5ace2039ad455c718
kim <kim@DESKTOP-T2AAIGR>
2024-01-05 17:59:46 +0900

    이분 탐색 활용

--- AlgorithmStudy_inflearn/src/Sorting_Searching/Inflearn6_10.java
+++ AlgorithmStudy_inflearn/src/Sorting_Searching/Inflearn6_10.java
@@ -1,41 +1,39 @@
 package Sorting_Searching;
 import java.util.*;
-class Inflearn6_9 {
-	public int count(int[] arr, int dist){
-		int cnt=1;
-		int ep=arr[0];
-		for(int i=1; i<arr.length; i++){
-			if(arr[i]-ep>=dist){
+class Inflearn6_10 {
+	public int count(int[] arr, int capacity){
+		int cnt=1, sum=0;
+		for(int x : arr){
+			if(sum+x > capacity){
 				cnt++;
-				ep=arr[i];
+				sum=x;
 			}
+			else sum+=x;
 		}
 		return cnt;
 	}
 
 	public int solution(int n, int m, int[] arr){
 		int answer=0;
-		Arrays.sort(arr);
-		int lt=1;
-		int rt=arr[n-1];
+		int lt=Arrays.stream(arr).max().getAsInt();
+		int rt=Arrays.stream(arr).sum();
 		while(lt<=rt){
 			int mid=(lt+rt)/2;
-			if(count(arr, mid)>=m){
+			if(count(arr, mid)<=m){
 				answer=mid;
-				lt=mid+1;
+				rt=mid-1;
 			}
-			else rt=mid-1;
+			else lt=mid+1;
 		}
 		return answer;
 	}
 	public static void main(String[] args){
-		Inflearn6_9 T = new Inflearn6_9();
+		Inflearn6_10 T = new Inflearn6_10();
 		Scanner sc = new Scanner(System.in);
 		int n=sc.nextInt();
 		int m=sc.nextInt();
 		int[] arr=new int[n];
-		for(int i=0; i<n; i++) 
-			arr[i]=sc.nextInt();
+		for(int i=0; i<n; i++) arr[i]=sc.nextInt();
 		System.out.println(T.solution(n, m, arr));
 	}
 }
