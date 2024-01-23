4044facd5b4a2dcc5248ef74da7b153fbdc9d3c7
minhyeok08 <ssklas08@gmail.com>
2024-01-23 22:41:33 +0900

    이진트리 레벨탐색(BFS)

--- AlgorithmStudy_inflearn/src/Recursive_Tree_Graph/Inflearn7_7.java
+++ AlgorithmStudy_inflearn/src/Recursive_Tree_Graph/Inflearn7_7.java
@@ -2,7 +2,7 @@ package Recursive_Tree_Graph;
 import java.util.*;
 class Node2{ 
     int data; 
-    Node lt, rt; 
+    Node2 lt, rt; 
     public Node2(int val) { 
         data=val; 
         lt=rt=null; 
@@ -10,16 +10,16 @@ class Node2{
 } 
   
 public class Inflearn7_7{ 
-    Node root; 
-    public void BFS(Node root){ 
-		Queue<Node> Q=new LinkedList<>();
+    Node2 root; 
+    public void BFS(Node2 root){ 
+		Queue<Node2> Q=new LinkedList<>();
 		Q.add(root);
 		int L=0;
         while(!Q.isEmpty()){
             int len = Q.size();
 			System.out.print(L+" : ");
             for(int i=0; i<len; i++){
-                Node cur = Q.poll();
+                Node2 cur = Q.poll();
                 System.out.print(cur.data+" ");
                 if(cur.lt!=null) Q.add(cur.lt);
                 if(cur.rt!=null) Q.add(cur.rt);
@@ -31,13 +31,13 @@ public class Inflearn7_7{
   
     public static void main(String args[]) { 
     	Inflearn7_7 tree=new Inflearn7_7(); 
-        tree.root=new Node(1); 
-        tree.root.lt=new Node(2); 
-        tree.root.rt=new Node(3); 
-        tree.root.lt.lt=new Node(4); 
-        tree.root.lt.rt=new Node(5); 
-		tree.root.rt.lt=new Node(6); 
-        tree.root.rt.rt=new Node(7);
+        tree.root=new Node2(1); 
+        tree.root.lt=new Node2(2); 
+        tree.root.rt=new Node2(3); 
+        tree.root.lt.lt=new Node2(4); 
+        tree.root.lt.rt=new Node2(5); 
+		tree.root.rt.lt=new Node2(6); 
+        tree.root.rt.rt=new Node2(7);
         tree.BFS(tree.root); 
     } 
 } 
