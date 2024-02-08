0ba3085011ca77ddd369d07c3b83741d5075400f
minhyeok08 <ssklas08@gmail.com>
2024-02-09 00:56:22 +0900

    미로탐색(DFS)

---
+++ AlgorithmStudy_inflearn/src/DFS_BFS_활용/Inflearn8_8.java
@@ -0,0 +1,31 @@
+package DFS_BFS_활용;
+import java.util.*;
+class Inflearn8_8{
+	static int[] combi;
+	static int n, m;
+	public void DFS(int L, int s)
+	{
+		if(L==m)
+		{
+			for(int x : combi) 
+				System.out.print(x+" ");
+			System.out.println();
+		}
+		else
+		{
+			for(int i=s; i<=n; i++)
+			{
+				combi[L] = i;
+				DFS(L+1, i+1);
+			}
+		}
+	}
+	public static void main(String[] args){
+		Inflearn8_8 T = new Inflearn8_8();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		combi = new int[m];
+		T.DFS(0, 1);
+	}
+}
