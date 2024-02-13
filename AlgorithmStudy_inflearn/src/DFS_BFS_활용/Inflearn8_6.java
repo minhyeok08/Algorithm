24e5928e34463c879424c06034fdb921cc26cf65
minhyeok08 <ssklas08@gmail.com>
2024-02-14 01:04:59 +0900

    순열 구하기

--- AlgorithmStudy_inflearn/src/DFS_BFS_활용/Inflearn8_6.java
+++ AlgorithmStudy_inflearn/src/DFS_BFS_활용/Inflearn8_6.java
@@ -1,54 +1,35 @@
 package DFS_BFS_활용;
 import java.util.*;
 class Inflearn8_6{
-	static int[] b, p, ch;
-	static int n, f;
-	boolean flag = false;
-	int[][] dy = new int[35][35];
-	public int combi(int n, int r)
-	{
-		if(dy[n][r]>0) 
-			return dy[n][r];
-		if(n==r || r==0) 
-			return 1;
-		else 
-			return dy[n][r] = combi(n-1, r-1) + combi(n-1, r);
-	}
-
-	public void DFS(int L, int sum){
-		if(flag) 
-			return;
-		if(L==n){
-			if(sum==f){
-				for(int x : p) 
-					System.out.print(x+" ");
-				flag=true;
-			}
+	static int[] pm, ch, arr;
+	static int n, m;
+	public void DFS(int L){
+		if(L==m){
+			for(int x : pm) 
+				System.out.print(x+" ");
+			System.out.println();
 		}
 		else{
-			for(int i=1; i<=n; i++){
+			for(int i=0; i<n; i++){
 				if(ch[i]==0){
-					ch[i]=1;
-					p[L]=i;
-					DFS(L+1, sum+(p[L]*b[L]));
-					ch[i]=0;
+					ch[i] = 1;
+					pm[L] = arr[i];
+					DFS(L+1);
+					ch[i] = 0;
 				}
 			}
 		}
 	}
-
 	public static void main(String[] args){
 		Inflearn8_6 T = new Inflearn8_6();
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
-		f = sc.nextInt();
-		b = new int[n];
-		p = new int[n];
-		ch = new int[n+1];
-		for(int i=0; i<n; i++)
-		{
-			b[i]=T.combi(n-1, i);
-		}
-		T.DFS(0, 0);
+		m = sc.nextInt();
+		arr = new int[n];
+		for(int i=0; i<n; i++) 
+			arr[i] = sc.nextInt();
+		ch = new int[n];
+		pm = new int[m];
+		T.DFS(0);
 	}
 }
