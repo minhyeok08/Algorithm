b13eb419df8cf1a938f56585ffcfa3e218472761
minhyeok08 <ssklas08@gmail.com>
2024-03-13 21:48:54 +0900

    최대점수 구하기

---
+++ AlgorithmStudy_inflearn/src/Dynamic_Programming/Inflearn10_6.java
@@ -0,0 +1,18 @@
+package Dynamic_Programming;
+import java.util.*;
+class Inflearn10_6{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[] dy = new int[m+1];
+		for(int i=0; i<n; i++){
+			int ps = sc.nextInt();
+			int pt = sc.nextInt();
+			for(int j=m; j>=pt; j--){
+				dy[j] = Math.max(dy[j], dy[j-pt] + ps);
+			}
+		}
+		System.out.print(dy[m]);
+	}
+}
