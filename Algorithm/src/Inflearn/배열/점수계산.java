fcdac4acfa357597a8478d921595851387181389
minhyeok08 <ssklas08@gmail.com>
2024-03-29 01:38:03 +0900

    arr

--- Algorithm/src/Inflearn/배열/점수계산.java
+++ Algorithm/src/Inflearn/배열/점수계산.java
@@ -24,8 +24,8 @@ public class 점수계산 {
 		점수계산 T = new 점수계산();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int[] arr = new int[n+1];
-		for(int i=1; i<n+1; i++)
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++)
 		{
 			arr[i] = sc.nextInt();
 		}
