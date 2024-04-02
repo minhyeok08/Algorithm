e2fca9db06469da275671198bbc26370eee6c382
minhyeok08 <ssklas08@gmail.com>
2024-04-02 21:30:12 +0900

    tp_sw5

--- Algorithm/src/Inflearn/TwoPointers_SlidingWindow/연속된자연수의합_TwoPointer.java
+++ Algorithm/src/Inflearn/TwoPointers_SlidingWindow/연속된자연수의합_TwoPointer.java
@@ -1,20 +1,30 @@
 package Inflearn.TwoPointers_SlidingWindow;
+// 7+8=15
+// 4+5+6=15
+// 1+2+3+4+5=15
+
 import java.util.*;
 class 연속된자연수의합_TwoPointer {	
 	public int Solution(int n){
-		int answer=0, sum=0;
-		int m=n/2+1;
-		int[] arr=new int[m];
-		for(int i=0; i<m; i++) arr[i]=i+1;
-		int lt=0;
-		for(int rt=0; rt<m; rt++){
-			sum+=arr[rt];
-			if(sum==n) answer++;
-			while(sum>=n){
-				sum-=arr[lt++];
-				if(sum==n) answer++; 
+		int answer = 0, sum = 0, lt = 1, len = n/2+1;
+		int[] arr = new int[len+1];
+		for(int i=1; i<=len; i++)
+		{
+			arr[i] = i;
+		}
+		for(int rt=1; rt<=len; rt++)
+		{
+			sum += arr[rt];
+			if(sum == n)
+				answer ++;
+			while(sum >= n)
+			{
+				sum -= arr[lt++];
+				if(sum == n)
+					answer ++;
 			}
 		}
+		
 		return answer;
 	}
 
