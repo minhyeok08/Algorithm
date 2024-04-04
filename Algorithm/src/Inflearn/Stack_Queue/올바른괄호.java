803b0ea7c1b16ebe0122bf6377b0798de260742c
minhyeok08 <ssklas08@gmail.com>
2024-04-05 00:38:17 +0900

    stack_queue

--- Algorithm/src/Inflearn/Stack_Queue/올바른괄호.java
+++ Algorithm/src/Inflearn/Stack_Queue/올바른괄호.java
@@ -1,25 +1,30 @@
 package Inflearn.Stack_Queue;
 
 import java.util.*;
-class 올바른괄호 {	
-	public String solution(String str){
+public class 올바른괄호 {	
+	public String Solution(String str){
 		String answer="YES";
-		Stack<Character> stack=new Stack<>();
-		for(char x : str.toCharArray()){
-			if(x=='(') stack.push(x);
-			else{
-				if(stack.isEmpty()) return "NO";
+		Stack<Character> stack = new Stack<>();
+		for(Character x : str.toCharArray())
+		{
+			if(x=='(')
+				stack.push(x);
+			else
+			{
+				if(stack.isEmpty())
+					return "NO";
 				stack.pop();
 			}
 		}
-		if(!stack.isEmpty()) return "NO";
+		if(!stack.isEmpty())
+			answer = "NO";
 		return answer;
 	}
 
 	public static void main(String[] args){
 		올바른괄호 T = new 올바른괄호();
 		Scanner sc = new Scanner(System.in);
-		String str=sc.next();
-		System.out.println(T.solution(str));
+		String str = sc.next();
+		System.out.println(T.Solution(str));
 	}
 }
