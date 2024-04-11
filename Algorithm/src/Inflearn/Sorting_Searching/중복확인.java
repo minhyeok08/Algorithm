b4830efba8c08eea03e3f07c06f9882adbbb2e39
minhyeok08 <ssklas08@gmail.com>
2024-04-11 21:32:52 +0900

    sort-search5

---
+++ Algorithm/src/Inflearn/Sorting_Searching/중복확인.java
@@ -0,0 +1,30 @@
+package Inflearn.Sorting_Searching;
+import java.util.*;
+public class 중복확인 {
+	public String Solution(int n, int[] arr)
+	{
+		String answer = "U";
+		Arrays.sort(arr);
+		for(int i=0; i<n-1; i++)
+		{
+			if(arr[i]==arr[i+1])
+			{
+				return "D";
+			}
+		}
+		return answer;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		중복확인 T = new 중복확인();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(T.Solution(n, arr));
+	}
+
+}
