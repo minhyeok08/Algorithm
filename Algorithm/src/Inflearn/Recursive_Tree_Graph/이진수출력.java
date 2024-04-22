6757f82412504b3bd2be8e8e8b819bc86b3f4247
minhyeok08 <ssklas08@gmail.com>
2024-04-22 22:30:31 +0900

    recursive2

---
+++ Algorithm/src/Inflearn/Recursive_Tree_Graph/이진수출력.java
@@ -0,0 +1,24 @@
+package Inflearn.Recursive_Tree_Graph;
+public class 이진수출력 {
+	public void DFS(int n)
+	{
+		if(n == 0)
+			return;
+		else
+		{
+			DFS(n/2);
+			System.out.print(n%2);
+		}
+		
+	}
+	public void Solution(int n)
+	{
+		DFS(n);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		이진수출력 T = new 이진수출력();
+		T.Solution(10);
+	}
+
+}
