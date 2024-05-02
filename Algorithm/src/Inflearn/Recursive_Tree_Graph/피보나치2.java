aa007a46b3deb3be7a2c8678072f9fa81ebd2705
minhyeok08 <ssklas08@gmail.com>
2024-05-02 23:45:32 +0900

    rtg4-2(memoization)

--- Algorithm/src/Inflearn/Recursive_Tree_Graph/피보나치2.java
+++ Algorithm/src/Inflearn/Recursive_Tree_Graph/피보나치2.java
@@ -3,12 +3,19 @@ public class 피보나치2 {
 	static int[] fibo;
 	public int DFS(int n){
 		if(fibo[n]>0) 
+			
 			return fibo[n];
+		
 		if(n==1) 
+			
 			return fibo[n]=1;
+		
 		else if(n==2) 
+			
 			return fibo[n]=1;
+		
 		else 
+			
 			return fibo[n]=DFS(n-2)+DFS(n-1);
 	}
 	
