4050b14524a6778d4c9fa28d5d9944f8e6e25812
minhyeok08 <ssklas08@gmail.com>
2024-05-25 22:09:56 +0900

    dfu

---
+++ Algorithm/src/DFS_BFS_활용/합이같은부분집합.java
@@ -0,0 +1,34 @@
+package DFS_BFS_활용;
+
+import java.util.*;
+class 합이같은부분집합{
+	static String answer="NO";
+	static int n, total=0;
+	boolean flag=false;
+	public void DFS(int L, int sum, int[] arr){
+		if(flag) return;
+		if(sum>total/2) return;
+		if(L==n){
+			if((total-sum)==sum){
+				answer="YES";
+				flag=true;
+			}	
+		}
+		else{
+			DFS(L+1, sum+arr[L], arr);
+			DFS(L+1, sum, arr);
+		}
+	}
+	public static void main(String[] args){
+		합이같은부분집합 T = new 합이같은부분집합();
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0; i<n; i++){
+			arr[i]=sc.nextInt();
+			total+=arr[i];
+		}
+		T.DFS(0, 0, arr);
+		System.out.println(answer);
+	}
+}
