b35b7f41f089edca2015f04b7e27956e1207fe9d
minhyeok08 <ssklas08@gmail.com>
2024-05-29 11:02:01 +0900

    dbu2

---
+++ Algorithm/src/Inflearn/DFS_BFS_활용/바둑이승차_DFS.java
@@ -0,0 +1,35 @@
+package Inflearn.DFS_BFS_활용;
+import java.util.*;
+public class 바둑이승차_DFS {
+	static int n, m, max;
+	public void DFS(int L, int sum, int[] arr)
+	{
+		if(L==m)
+		{
+			if(max < sum && n >= sum)
+			{
+				max = sum;
+			}
+		}
+		else
+		{
+			DFS(L+1, sum+arr[L], arr);
+			DFS(L+1, sum, arr);
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		바둑이승차_DFS T = new 바둑이승차_DFS();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();
+		int[] arr = new int[m];
+		for(int i=0; i<m; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		T.DFS(0, 0, arr);
+		System.out.println(max);
+	}
+
+}
