1d4dd46c80fa0b093cec655a3f3bce4c2be3dcb1
minhyeok08 <ssklas08@gmail.com>
2024-05-30 22:28:50 +0900

    dbu6 // check 배열 이용 중복체크

---
+++ Algorithm/src/Inflearn/DFS_BFS_활용/순열구하기.java
@@ -0,0 +1,51 @@
+package Inflearn.DFS_BFS_활용;
+import java.util.*;
+// L = 0 / pm[0] = 3
+// L = 1 / pm[1] = 3
+// L = 2 / 
+
+public class 순열구하기 {
+	static int n, m;
+	static int[] pm, ch;
+	public void DFS(int L, int[] arr)
+	{
+		if(L==m)
+		{
+			for(int i=0; i<m; i++)
+			{
+				System.out.print(pm[i]+" ");
+			}
+			System.out.println();
+		}
+		else
+		{
+			for(int i=0; i<n; i++)
+			{
+				if(ch[i] == 0)
+				{
+					ch[i] = 1;
+					pm[L] = arr[i];
+					DFS(L+1, arr);
+					ch[i] = 0;
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		순열구하기 T = new 순열구하기();
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		m = sc.nextInt();	
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++)
+		{
+			arr[i] = sc.nextInt();
+		}
+		ch = new int[n];	
+		pm = new int[m];
+		T.DFS(0, arr);
+		
+	}
+
+}
