6d25827db5b359a56394fbe304afab6779c16c96
minhyeok08 <ssklas08@gmail.com>
2024-06-19 03:16:51 +0900

    ga3 // 정렬 예제 활용 2

--- Algorithm/src/Inflearn/GreedyAlgorithm/회의실배정.java
+++ Algorithm/src/Inflearn/GreedyAlgorithm/회의실배정.java
@@ -9,15 +9,32 @@ class Time implements Comparable<Time>{
 		this.e = e;
 	}
 	@Override
-	public int compareTo(Time O)
+	public int compareTo(Time O)	// 정렬부분이 Point!!
 	{
-		return this.e - O.e; // 음수 오름차순
+		if(this.e == O.e)	// 종료시간이 같다면 => 시작시간을 오름차순 정렬 => 종료시간이 같다면 더 이른 시작시간을 가진 회의가 들어가야하므로
+		{
+			return this.s - O.s;  // 음수 => 시작시간 오름차순	
+		}
+		else
+		{
+			return this.e - O.e; // 음수 => 종료시간 오름차순			
+		}
 	}
 }
 public class 회의실배정 {
-	public int Solution()
+	static int cnt = 0;
+	public void Solution(ArrayList<Time> arr)
 	{
-		return 1;
+		int end = 0;
+		Collections.sort(arr);
+		for(Time ob : arr)
+		{
+			if(ob.s >= end)	// 시작시간이 종료시간과 같거나 뒤늦은 시간이라면
+			{
+				cnt++;
+				end = ob.e;	// 회의실 종료시간을 끝나는 시간을 초기화
+			}
+		}
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -31,6 +48,8 @@ public class 회의실배정 {
 			int e = sc.nextInt();
 			arr.add(new Time(s, e));
 		}
+		T.Solution(arr);
+		System.out.println(cnt);
 	}
 
 }
